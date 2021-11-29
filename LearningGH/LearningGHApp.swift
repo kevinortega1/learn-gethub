faccce7959eb491dcee94573de90d0433d89d2f2
kevinortega1 <okevin930@gmail.com>
2021-11-29 13:00:31 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by PM Perl - Kevin Ortega on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
